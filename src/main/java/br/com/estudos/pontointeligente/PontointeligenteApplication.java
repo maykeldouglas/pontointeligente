4d9eed9a0b87cd16d4851ec5cc2a09e877c997e0
maykel <maykel.silva@kaspper.com>
2020-10-24 09:02:06 -0300

    Commit inicial

---
+++ src/main/java/br/com/estudos/pontointeligente/PontointeligenteApplication.java
@@ -0,0 +1,13 @@
+package br.com.estudos.pontointeligente;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PontointeligenteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PontointeligenteApplication.class, args);
+    }
+
+}
